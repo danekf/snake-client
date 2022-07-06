a9ef4bc85a3f71a1aaf5b3ef051c4aa8e0b942e9
Danek <danek.fill@outlook.com>
2022-07-06 17:41:54 +0000

    reuploading changes

--- client.js
+++ client.js
@@ -1,14 +1,23 @@
 // establishes a connection with the game server
+const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
-    host: 165.227.47.243 ,
-    port: 50541
+    host: "165.227.47.243" ,
+    port: "50541"
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //start console logging data once connection is established
+  conn.on("connect", (data) => {
+    console.log(data);
+  });
+
   return conn;
+
+  
 };
 
-console.exports= connect;
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const net = require("net");
-//const connect = require("client.js");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243" ,
-    port: "50541"
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //start console logging data once connection is established
-  conn.on("connect", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-
-  
-};
+const client = require("./client.js")
+// const {connect} = require("client.js");
 
 console.log("Connecting ...");
-connect();
+client.connect();
 
 
