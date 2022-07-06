1f2f5389e6334c89101bd41b463e11b184a6efc1
Danek <danek.fill@outlook.com>
2022-07-06 17:25:58 +0000

    fixes to logging

--- play.js
+++ play.js
@@ -1,10 +1,26 @@
 const net = require("net");
-const connect = require("client.js");
+//const connect = require("client.js");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243" ,
+    port: "50541"
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //start console logging data once connection is established
+  conn.on("connect", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+
+  
+};
 
 console.log("Connecting ...");
 connect();
 
-//start console logging data once connection is established
-conn.on("connect", (data) => {
-  console.log(data);
-});
+
