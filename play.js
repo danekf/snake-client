020f6a3e2272842b552c276d91691b6930a264f2
Danek <danek.fill@outlook.com>
2022-07-06 18:03:58 +0000

    starting to add user input, CTRL+C for now

--- play.js
+++ play.js
@@ -1,7 +1,27 @@
 const client = require("./client.js")
-// const {connect} = require("client.js");
+
+//handling input
+const handleUserInput = function () {
+  
+  //exit if CTRL C is pressed
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+
+//given setup by LHL
+const setupInput = function () {
+  stdin.on("data", handleUserInput);
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 
 console.log("Connecting ...");
 client.connect();
-
-
+//setupInput();
